a4cc3d0ed8c4a5bc2ff55164a9c597084a0f0827
Przemysław Żydek <przemyslawzydek@gmail.com>
2022-07-10 10:20:32 +0200

    build: add deployment setup

---
+++ deploy/src/lightsail.ts
@@ -0,0 +1,65 @@
+import * as pulumi from '@pulumi/pulumi';
+import * as aws from '@pulumi/aws';
+import * as awsx from '@pulumi/awsx';
+import { projectRoot } from './data';
+import * as path from 'path';
+
+const stack = pulumi.getStack();
+const name = pulumi.getProject();
+const serviceName = `${stack}-${name}`;
+
+const ecr = new aws.ecr.Repository(`${serviceName}-server`, {
+  imageScanningConfiguration: {
+    scanOnPush: false,
+  },
+});
+
+const server = new awsx.ecr.Image(`${serviceName}-server`, {
+  path: projectRoot,
+  dockerfile: path.join(projectRoot, 'docker/server/Dockerfile'),
+  repositoryUrl: ecr.repositoryUrl,
+  env: {
+    DOCKER_BUILDKIT: '1',
+  },
+});
+
+export const repoName = ecr.name;
+export const repoUrl = ecr.repositoryUrl;
+export const serverImageUri = server.imageUri;
+
+const containerService = new aws.lightsail.ContainerService(serviceName, {
+  isDisabled: false,
+  power: 'nano',
+  scale: 1,
+  name: serviceName,
+});
+
+const port = 3000;
+const containerServiceDeployment =
+  new aws.lightsail.ContainerServiceDeploymentVersion(serviceName, {
+    publicEndpoint: {
+      containerName: ecr.name,
+      containerPort: port,
+      healthCheck: {
+        path: '/',
+        timeoutSeconds: 4,
+      },
+    },
+    containers: [
+      {
+        containerName: ecr.name,
+        image: server.imageUri,
+        ports: {
+          [port]: 'HTTP',
+        },
+        environment: {
+          BOT_TOKEN: process.env.BOT_TOKEN as string,
+          PORT: port.toString(),
+        },
+      },
+    ],
+    serviceName: containerService.name,
+  });
+
+export const publicEndpoint = containerServiceDeployment.publicEndpoint;
+export const deploymentState = containerServiceDeployment.state;
