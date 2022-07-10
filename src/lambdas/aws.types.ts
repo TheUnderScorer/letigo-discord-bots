de40c1a9c1a36be543447a64da65775bf1cc85d0
Przemysław Żydek <przemyslawzydek@gmail.com>
2022-07-10 15:45:09 +0200

    feat: Remove lambdas
    BREAKING CHANGE: API Endpoints are no longer available

--- src/lambdas/aws.types.ts
+++
@@ -1,3 +0,0 @@
-import { APIGatewayProxyStructuredResultV2 } from 'aws-lambda/trigger/api-gateway-proxy';
-
-export const response = (data: APIGatewayProxyStructuredResultV2) => data;
--- src/lambdas/commandHandlers/commandHandlers.ts
+++
@@ -1,8 +0,0 @@
-import { CommandHandler, Commands } from '../../server/commands/command.types';
-import { kolegoHandler } from './kolego/kolego.handler';
-import { coTamHandler } from './coTam/coTam.handler';
-
-export const commandHandlers: Record<Commands, CommandHandler> = {
-  [Commands.Kolego]: kolegoHandler,
-  [Commands.CoTam]: coTamHandler,
-};
