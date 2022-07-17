a921990174e35493b88df0c2101c9be6effd994a
Przemysław Żydek <przemyslawzydek@gmail.com>
2022-07-17 14:22:46 +0200

    feat: support new daily report format

--- src/server/scheduler/jobs/dailyReport/dailyReportReminder.ts
+++ src/server/scheduler/jobs/dailyReport/dailyReportReminder.ts
@@ -30,7 +30,7 @@ export const createDailyReportReminder = (
 });
 
 export const isDailyReport = (content: string) =>
-  content.toLowerCase().startsWith('[dzień');
+  content.split('\n')[0].toLowerCase().includes('[dzień');
 
 export const isValidAuthor = (message: Message, targetUserId: string) =>
   message.author?.id === targetUserId;
