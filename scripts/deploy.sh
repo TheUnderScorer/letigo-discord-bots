204118a5b985d7cfa994284fffeab6c24588b428
Przemysław Żydek <przemyslawzydek@gmail.com>
2025-04-15 20:18:08 +0200

    build: use docker compose

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,10 +2,10 @@
 set -e
 
 echo "[INFO] Pulling latest image..."
-docker-compose pull prod
+docker compose pull prod
 
 echo "[INFO] Recreating container..."
-docker-compose up -d prod
+docker compose up -d prod
 
 echo "[INFO] Removing unused images..."
 docker image prune -f
